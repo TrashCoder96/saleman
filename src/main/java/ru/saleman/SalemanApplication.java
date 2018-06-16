d56a420431769138046191be6031df803c63a589
Ivan <kiberaction@yandex.ru>
2018-06-17 02:08:45 +0300

    Add test data

--- src/main/java/ru/saleman/SalemanApplication.java
+++ src/main/java/ru/saleman/SalemanApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.saleman.data.CredentialsRepository;
+import ru.saleman.data.MicrosystemRepository;
 import ru.saleman.data.RoleRepository;
 import ru.saleman.data.dto.CredentialsDto;
+import ru.saleman.data.dto.MicrosystemDto;
 import ru.saleman.util.SalemanConstants;
 
 import javax.annotation.PostConstruct;
@@ -20,14 +22,21 @@ public class SalemanApplication {
 	@Autowired
 	private RoleRepository roleRepository;
 
+	@Autowired
+	private MicrosystemRepository microsystemRepository;
+
 	@PostConstruct
 	@Transactional
 	public void post() {
+		MicrosystemDto microsystemDto = new MicrosystemDto();
+		microsystemDto.setName("ivan_company");
 		CredentialsDto cs = new CredentialsDto();
 		cs.setPassword("ivan");
 		cs.setUsername("ivan");
 		cs.setRole(roleRepository.findById(SalemanConstants.Role.USER).get());
 		credentialsRepository.save(cs);
+		microsystemDto.setAdmin(cs);
+		microsystemRepository.save(microsystemDto);
 	}
 
 	public static void main(String[] args) {
