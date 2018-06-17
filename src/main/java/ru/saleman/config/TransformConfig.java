1606352385d37de3a3e42f0a90d2cd241bc895b6
Ivan <kiberaction@yandex.ru>
2018-06-17 14:48:05 +0300

    Refactoring of converters

--- src/main/java/ru/saleman/config/TransformConfig.java
+++ src/main/java/ru/saleman/config/TransformConfig.java
@@ -1,10 +1,12 @@
 package ru.saleman.config;
 
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.saleman.manager.converter.AccountDtoVoConverter;
+
+import java.util.List;
 
 /**
  * Created by Ivan Timofeev on 10.06.2018.
@@ -13,12 +15,14 @@ import ru.saleman.manager.converter.AccountDtoVoConverter;
 public class TransformConfig {
 
     @Autowired
-    private AccountDtoVoConverter accountDtoVoConverter;
+    private List<AbstractConverter> converters;
 
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.addConverter(accountDtoVoConverter);
+        converters.stream().forEach(converter -> {
+            modelMapper.addConverter(converter);
+        });
         return modelMapper;
     }
 
